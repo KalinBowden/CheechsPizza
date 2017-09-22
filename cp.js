baa67120551896615582d4ff01637708e61d6a5d
Kalin Bowden <KalinBowden@gmail.com>
2017-09-22 16:40:57 -0500

    added add pizza function

--- cp.js
+++ cp.js
@@ -7,7 +7,7 @@ Date:
 var custName = "John Smith";
 var custPhone;
 var subTotal = 0.0;
-var taxRate = 0.06;
+var taxRate = 7.6;
 var orderTotal = 0.0;
 
 
@@ -25,20 +25,75 @@ function getCustomerInfo()
     custPhone = document.getElementById("custPhone").value;
 }
 
-// add the different falvors of pizza
+
+// Add the different falvors of pizza
 function addFlavors()
 {
+    var testString;
+
     for (var index = 0; index < pizzaTypes.length; index++)
     {
-        testString += pizzaTypes[index] + " - " + pizzaPrices[index];
+        testString += '<option>' + pizzaTypes[index] + ' - ' + pizzaPrices[index] + '</option>';
     }
+
+    return testString;
 }
+
 
 // Add the number of choices;
 function addNumChoices()
 {
+    var testString = "";
+
     for (var index = 0; index < 10; index++)
     {
-        testString += '<option>' + (index + 1) +'</opyion>';
+        testString += '<option>' + (index + 1) +'</option>';
     }
+
+    return testString;
+}
+
+
+// Build the coloumn for the pizzaOptions
+function buildPizzaColoumn()
+{
+    var testString = "";
+
+    testString = '<select class="form-control">' + addFlavors() + '</select>';
+
+    return testString;
+}
+
+
+// Build the coloumn for number of pizzas
+function buildNumberColoumn()
+{
+    var testString = "";
+    
+    testString = '<select class="form-control">' + addNumChoices() + '</select>';
+    
+    return testString;
+}
+
+
+// Write a pizza selection row to the page
+function buildSelectionRow()
+{
+    var testString = "";
+
+    testString = '<div class="row pizzaBox">' +
+                    '<div class="col-md-8">' +
+                        buildPizzaColoumn() +
+                    '</div>' +
+                    '<div class="col-md-3">' +
+                        buildNumberColoumn() +
+                    '</div>' +
+                    '<div class="col-md-1">' +
+                        '<span class="glyphicon glyphicon-plus glify" aria-hidden="true"></span>' +
+                    '</div>' +
+                '</div>';
+
+    alert(testString);
+    document.write(testString);
+
 }
